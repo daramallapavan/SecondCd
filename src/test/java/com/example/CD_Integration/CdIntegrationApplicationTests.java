7525beadefff7d2b82dc8a008fd74ad1ef014167
daramallapavan <daramallapavan123@gmail.com>
2024-10-01 23:06:20 +0530

    first commit

---
+++ src/test/java/com/example/CD_Integration/CdIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CD_Integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
